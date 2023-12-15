fab19e683b5b4117ef2859e177f5d994f149aebc
Alexander Golyshev <124289166+a-lournrose@users.noreply.github.com>
2023-12-15 21:26:10 +0300

    страница моей компании

---
+++ src/components/entities/company/company-events/company-events.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import EventCard from '@components/entities/company/company-events/event-card';
+
+const CompanyEvents = () => {
+  return (
+    <div className='mt-6'>
+      <EventCard className='mb-[72px]'/>
+      <EventCard className='mb-[72px]'/>
+      <EventCard className='mb-[72px]'/>
+      <EventCard className='mb-[72px]'/>
+      <EventCard className='mb-[72px]'/>
+    </div>
+  );
+};
+
+export default CompanyEvents;
