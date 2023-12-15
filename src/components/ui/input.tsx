0c1a282683cbf8e7859c3e711787a1b35354dce7
DenisRybkin <denis.rybkin.94@mail.ru>
2023-12-15 18:16:53 +0300

    fxi

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,12 +4,12 @@ import { cn } from '@lib/utils/tools/cn';
 import { cva, VariantProps } from 'class-variance-authority';
 
 const inputVariants = cva(
-  'cursor-pointer border-none flex h-10 w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm  file:text-blue file:border-0 file:bg-transparent file:text-sm file:font-medium file:!text-primary-500 file:overflow-ellipsis placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-dark-1 dark:placeholder:text-slate-400 ',
+  'cursor-pointer text-black flex h-10 w-full rounded-md border border-gray focus:border-none bg-white px-3 py-2 text-sm  file:text-blue file:border-0 file:bg-transparent file:text-sm file:font-medium file:!text-primary-500 file:overflow-ellipsis placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-dark-1 dark:placeholder:text-slate-400 ',
   {
     variants: {
       variant: {
         default:
-          'ring-offset-white focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 dark:outline-none dark:ring-offset-slate-950 dark:focus-visible:ring-primary-500',
+          'ring-offset-white focus-visible:ring-2 focus-visible:ring-gray focus-visible:ring-offset-2 dark:outline-none dark:ring-offset-slate-950 dark:focus-visible:ring-primary-500',
         ghost: 'dark:focus-visible:ring-0 shadow-none ring-offset-0',
       },
       size: {
